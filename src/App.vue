30a05a7d6ce944d5fb05f71d4673955889231ee7
Dane Horn <dane.horn7915@gmail.com>
2020-03-08 14:30:31 +0200

    add arcade classic font and app header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <v-app>
-    <v-container>
-      <v-row>
-        <v-col cols="2">
-          <ConnectButton />
-        </v-col>
-        <v-col cols="2">
-          <PlayerNameInput />
-        </v-col>
-        <v-col cols="2">
-          <TilesetSelect v-on:changed="updateTileset" />
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="2">
-          <PlayerList />
-        </v-col>
-        <v-col>
-          <Map :tileset="tileset"></Map>
-        </v-col>
-        <v-col cols="2"></v-col>
-      </v-row>
-    </v-container>
-  </v-app>
+  <div id="app2">
+    <v-app-bar dense dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>BBD RPG</v-toolbar-title>
+    </v-app-bar>
+    <v-app>
+      <v-container>
+        <v-row>
+          <v-col cols="2">
+            <ConnectButton />
+          </v-col>
+          <v-col cols="2">
+            <PlayerNameInput />
+          </v-col>
+          <v-col cols="2">
+            <TilesetSelect v-on:changed="updateTileset" />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="2">
+            <PlayerList />
+          </v-col>
+          <v-col>
+            <Map :tileset="tileset"></Map>
+          </v-col>
+          <v-col cols="2"></v-col>
+        </v-row>
+      </v-container>
+    </v-app>
+  </div>
 </template>
 
 <script>
@@ -57,8 +63,17 @@ export default {
 </script>
 
 <style>
+@font-face {
+  font-family: "ArcadeClassic";
+  font-style: normal;
+  font-weight: normal;
+  src: local("ArcadeClassic"), url("assets/ARCADECLASSIC.woff") format("woff");
+}
+#app2 {
+  font-family: ArcadeClassic;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: ArcadeClassic;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
