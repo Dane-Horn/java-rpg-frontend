6e8e5692b191710b8be5af9fb8e071fbe75dd071
Dane Horn <dane.horn7915@gmail.com>
2020-03-10 23:18:14 +0200

    replicate most app bar functionality using vuetify components and without bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,75 @@
 <template>
   <div id="app2">
-    <v-app-bar elevation="0" style="background-color: #FFFFFF">
-      <v-col cols="1">
-        <v-img
-          src="https://i.ibb.co/rp1Rdd7/bbd-logo.png"
-          max-height="50px"
-          max-width="50px"
-        ></v-img>
+    <v-app-bar
+      style="text-align: center; font-size: 30px; border-bottom: 5px solid black; background-color: #FFFFFF"
+    >
+      <v-img
+        @click.stop="drawer = !drawer"
+        src="https://i.ibb.co/rp1Rdd7/bbd-logo.png"
+        max-height="50px"
+        max-width="50px"
+      ></v-img>
+      <v-col>
+        BBD RPG
       </v-col>
-      <v-col cols="2">
-        <v-toolbar-title>BBD RPG</v-toolbar-title>
+      <v-col>
+        <router-link class="link" to="/home">Home</router-link>
+      </v-col>
+      <v-col cols="1">
+        <router-link class="link" to="/profile">Profile</router-link>
+      </v-col>
+      <v-col>
+        <router-link class="link" to="/maze">Maze</router-link>
+      </v-col>
+      <v-spacer></v-spacer>
+      <v-col>
+        <router-link class="link" to="/login">Login</router-link>
+      </v-col>
+      <v-col>
+        <router-link class="link" to="/signup">Signup</router-link>
       </v-col>
     </v-app-bar>
+    <v-navigation-drawer v-model="drawer" temporary absolute>
+      <v-list nav dense>
+        <v-list-item-group active-class="deep-purple--text text--accent-4">
+          <router-link class="link" to="/home">
+            <v-list-item>
+              <v-list-item-title>
+                Home
+              </v-list-item-title>
+            </v-list-item>
+          </router-link>
+          <router-link class="link" to="/profile">
+            <v-list-item>
+              <v-list-item-title>
+                Profile
+              </v-list-item-title>
+            </v-list-item>
+          </router-link>
+          <router-link class="link" to="/maze">
+            <v-list-item>
+              <v-list-item-title>
+                Maze
+              </v-list-item-title>
+            </v-list-item>
+          </router-link>
+          <router-link class="link" to="/login">
+            <v-list-item>
+              <v-list-item-title>
+                Login
+              </v-list-item-title>
+            </v-list-item>
+          </router-link>
+          <router-link class="link" to="/signup">
+            <v-list-item>
+              <v-list-item-title>
+                Signup
+              </v-list-item-title>
+            </v-list-item>
+          </router-link>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
     <router-view></router-view>
   </div>
 </template>
@@ -24,6 +82,7 @@ export default {
   components: {},
   data() {
     return {
+      drawer: false,
       tileset: ""
     };
   },
@@ -36,6 +95,13 @@ export default {
 </script>
 
 <style>
+.link {
+  text-decoration: none;
+  color: black;
+}
+.link:hover {
+  color: red;
+}
 @font-face {
   font-family: "ArcadeClassic";
   font-style: normal;
--- src/main.js
+++ src/main.js
@@ -1,22 +1,22 @@
-import Vue from 'vue';
-import App from './App.vue';
-import { store } from './store/store';
-import vuetify from './plugins/vuetify';
-import VueRouter from 'vue-router';
+import Vue from "vue";
+import App from "./App.vue";
+import { store } from "./store/store";
+import vuetify from "./plugins/vuetify";
+import VueRouter from "vue-router";
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
 
-import { MapScreen, LoginScreen } from './components/index';
+import { MapScreen, LoginScreen } from "./components/index";
 const router = new VueRouter({
   routes: [
-    { path: '/login', component: LoginScreen },
-    { path: '/play', component: MapScreen }
+    { path: "/login", component: LoginScreen },
+    { path: "/maze", component: MapScreen }
   ]
-})
+});
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
